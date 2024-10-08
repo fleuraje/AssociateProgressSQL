01e55fcb41793043c69184205af35ab0adc19f3b
Jeremiah F. <127205495+fleuraje@users.noreply.github.com>
2024-10-08 14:30:39 -0600

    added more employes to table, that work on OP and MECH as well.

--- Associate_Progress/Queries.sql
+++ Associate_Progress/Queries.sql
@@ -1,1 +1,11 @@
 SELECT * FROM Employee_Info;
+
+--changing username in table, samething but just preference, and practice using RENAME query --
+ALTER TABLE employee_progress
+RENAME COLUMN username TO login;
+-- also name change--
+ALTER TABLE employee_progress
+RENAME COLUMN last_name TO time_in_path;
+--now i want to add more columns of data 
+ALTER TABLE employee_progress 
+ADD COLUMN total_units_picked INT;
---
+++ Associate_Progress/UPDATE time_in_path.sql
@@ -0,0 +1,3 @@
+UPDATE time_in_path
+WHERE login ='fleuraje'
+set Doe = 8.3;
