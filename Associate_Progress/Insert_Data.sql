e71c01224881c299ff46981fca22a7dfdfeb945d
Jeremiah F. <127205495+fleuraje@users.noreply.github.com>
2024-10-08 20:17:51 -0600

    getting ready to add front end data to project

--- Associate_Progress/Insert_Data.sql
+++ Associate_Progress/Insert_Data.sql
@@ -1,2 +1,25 @@
 INSERT INTO Employee_Info (username, first_name, last_name, Pit_trained_machine, rate)
 VALUES ('fleuraje', 'Jeremiah', 'Doe', 'OP', 150);
+--updated variables below--
+INSERT INTO employee_progress (login , first_name, time_in_path, Pit_trained_machine, rate, total_units_picked)
+VALUES ('fleuraje', 'Jeremiah', 8.5, 'OP', 21.0, 200);
+--8.5 hrs , 21.0 uph, 200 units picked total--
+INSERT INTO employee_progress (login , first_name, time_in_path, Pit_trained_machine, rate, total_units_picked)
+VALUES 
+('Giselfro', 'Gisela', 8.45, 'OP', 24.14, 204),
+('Jesrap', 'Jesus', 2.57, 'OP', 26.83, 69),
+('Kfrlopz', 'Francisco', 1.27, 'OP', 24.94, 33),
+('Petroar', 'Nellz', 8.39, 'OP', 23.72, 199),
+('Rgbritan', 'Britany', 8.78, 'OP', 21.2, 186),
+('Julisdav', 'Julissa', 6.98, 'OP', 22.21, 155),
+('Segugu', 'Gina', 2.3, 'OP', 17.37, 40),
+('Landeu', 'Stanley', 2.96, 'OP', 21.95, 65),
+('Romgob', 'Rigo', 1.01, 'OP', 13.86, 14),
+('Cheric', 'Eric', 5.92, 'OP', 15.7, 93),
+('amrtga', 'Martha', 5.94 , 'MECH', 17.5, 104),
+('yampets', 'Peter', 7.91, 'MECH', 12.38, 98),
+('bellga', 'Jeff', 1.47, 'MECH', 19, 28),
+('Mattyv', 'Matt', 3.84, 'MECH', 13.29, 51),
+('chaxfell', 'Charley', 2.42, 'MECH',14.43, 35),
+;
+
